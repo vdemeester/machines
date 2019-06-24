ec94501488be81980827dbaba237c9f95bcd6d3a
Vincent Demeester <vincent@sbr.pm>
2019-06-24 13:23:59 +0200

    Use ansible 👼
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- shell.nix
+++ shell.nix
@@ -3,6 +3,10 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "nixbox-shell";
   buildInputs = [
+    ansible
+    ansible-lint
+    python37
+    python37Packages.libvirt
     gnumake
     packer
   ];
